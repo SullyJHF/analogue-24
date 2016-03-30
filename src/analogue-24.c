22a86c100a5f41fe672c7e9fd3871e9389e38b9e
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-03-30 21:10:30 +0100

    Refactor line end code

--- src/analogue-24.c
+++ src/analogue-24.c
@@ -12,20 +12,27 @@ static GPoint minute_end;
 #define MINUTE_LENGTH 65
 
 static GPoint hour_end;
-#define HOUR_LENGTH 40
+#define HOUR_LENGTH 45
+
+static GPoint lower_dot;
 
 /****** drawing stuff ******/
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
-  // Set the line color to white for the minute hand
+  graphics_context_set_stroke_width(ctx, 9);
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  // Set the stroke width
-  graphics_context_set_stroke_width(ctx, 7);
+  graphics_draw_line(ctx, lower_dot, lower_dot);
+
   graphics_draw_line(ctx, centre, minute_end);
 
   graphics_context_set_stroke_color(ctx, GColorRed);
   graphics_draw_line(ctx, centre, hour_end);
+}
 
-  // Set the line color to red for the hour hand
+static GPoint set_end(int32_t angle, int length) {
+  GPoint result;
+  result.y = (-cos_lookup(angle) * length / TRIG_MAX_RATIO) + centre.y;
+  result.x = (sin_lookup(angle) * length / TRIG_MAX_RATIO) + centre.x;
+  return result;
 }
 
 /****** tick handler ******/
@@ -33,36 +40,34 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   int hour = tick_time->tm_hour;
   int minute = tick_time->tm_min;
 
-  int32_t minute_angle = TRIG_MAX_ANGLE * minute / 60;
-  minute_end.y = (-cos_lookup(minute_angle) * MINUTE_LENGTH / TRIG_MAX_RATIO) + centre.y;
-  minute_end.x = (sin_lookup(minute_angle) * MINUTE_LENGTH / TRIG_MAX_RATIO) + centre.x;
+  int32_t minute_frac = TRIG_MAX_ANGLE * minute / 60;
+  int32_t hour_frac = TRIG_MAX_ANGLE * hour / 24;
 
-  int32_t hour_angle = TRIG_MAX_ANGLE * hour / 24;
-  hour_end.y = (-cos_lookup(hour_angle) * HOUR_LENGTH / TRIG_MAX_RATIO) + centre.y;
-  hour_end.x = (sin_lookup(hour_angle) * HOUR_LENGTH / TRIG_MAX_RATIO) + centre.x;
+  minute_end = set_end(minute_frac, MINUTE_LENGTH);
+  hour_end = set_end(hour_frac, HOUR_LENGTH);
 
   layer_mark_dirty(main_clock_layer);
 }
 
+/****** window life ******/
 static void window_load(Window *window) {
   centre = GPoint(WIDTH / 2, HEIGHT / 2);
+  lower_dot = GPoint(WIDTH / 2, 158);
+
 
   minute_end = centre;
   hour_end = centre;
-  // get the root layer
+
   Layer *window_layer = window_get_root_layer(window);
-  // get the bounds of the root layer
   GRect bounds = layer_get_bounds(window_layer);
-  // create the main clock layer
+
   main_clock_layer = layer_create(bounds);
-  // set the update procedure
+
   layer_set_update_proc(main_clock_layer, canvas_update_proc);
-  // add the main clock layer to the root layer
   layer_add_child(window_layer, main_clock_layer);
 }
 
 static void window_unload(Window *window) {
-  // destroy the main clock layer on exit
   layer_destroy(main_clock_layer);
 }
 
@@ -73,9 +78,8 @@ static void init(void) {
     .load = window_load,
     .unload = window_unload,
   });
-  const bool animated = true;
-  window_stack_push(window, animated);
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  window_stack_push(window, true);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   layer_mark_dirty(main_clock_layer);
 }
 
